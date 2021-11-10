b5e33a743b60c79bcb357dc9ad1ba03cf54c5ca5
tenkoh <tenkoh@example.com>
2021-11-10 14:07:14 +0900

    func WalkDir, CopyTree

--- main.go
+++ main.go
@@ -8,10 +8,17 @@ This package includes some utility functions to operate files and directories.
 package fop
 
 import (
+	"errors"
 	"fmt"
+	"io"
 	"os"
 	"path/filepath"
 	"strings"
+)
+
+var (
+	ErrFailedCreatingDirectory = errors.New("failed to create a directory")
+	ErrInvalidDestination      = errors.New("destination must be a directory")
 )
 
 // ParentDir returns the parent directory's path in absolute type.
@@ -30,4 +37,104 @@ func ParentDir(path string) (string, error) {
 	}
 	parent := filepath.Dir(p)
 	return parent, nil
+}
+
+func CopyTree(src, dst string) ([]string, error) {
+	// check dst is not a file. dst must be a directory.
+	info, err := os.Stat(dst)
+	if err != nil {
+		if !info.IsDir() {
+			return nil, ErrInvalidDestination
+		}
+	}
+	// if src is a file, just copy it into dst
+	info, err = os.Stat(src)
+	if err != nil {
+		return nil, err
+	}
+	if !info.IsDir() {
+		savepath := filepath.Join(dst, filepath.Base(src))
+		if err := copyFile(src, savepath); err != nil {
+			return nil, err
+		}
+		return []string{savepath}, nil
+	}
+
+	// if src is a directory, find all files and copy all.
+	files, err := WalkFiles(src)
+	if err != nil {
+		return nil, err
+	}
+	copied := []string{}
+	for _, file := range files {
+		rel, err := filepath.Rel(src, file)
+		if err != nil {
+			return nil, err
+		}
+		savepath := filepath.Join(dst, rel)
+		if err := copyFile(file, savepath); err != nil {
+			return nil, err
+		}
+		copied = append(copied, savepath)
+	}
+	return copied, nil
+}
+
+// src, dst must be filepath, not directory.
+func copyFile(src, dst string) error {
+	dir := filepath.Dir(dst)
+	_, err := os.Stat(dir)
+	if err != nil {
+		if err := os.MkdirAll(dir, 0777); err != nil {
+			return err
+		}
+	}
+	f, err := os.Open(src)
+	if err != nil {
+		return err
+	}
+	defer f.Close()
+	g, err := os.Create(dst)
+	if err != nil {
+		return err
+	}
+	defer g.Close()
+	if _, err := io.Copy(g, f); err != nil {
+		return err
+	}
+	return nil
+}
+
+// WalkFiles returns a file tree under "dir".
+//
+// Empty directories are ignored.
+func WalkFiles(dir string) ([]string, error) {
+	// when filepath is passed, return it.
+	info, err := os.Stat(dir)
+	if err != nil {
+		return nil, err
+	}
+	if !info.IsDir() {
+		// using filepath.Join to fit output path style.
+		return []string{filepath.Join(dir)}, nil
+	}
+
+	paths := []string{}
+	files, err := os.ReadDir(dir)
+	if err != nil {
+		return nil, err
+	}
+	for _, file := range files {
+		fullpath := filepath.Join(dir, file.Name())
+		if !file.IsDir() {
+			paths = append(paths, fullpath)
+			continue
+		}
+		subpaths, err := WalkFiles(fullpath)
+		if err != nil {
+			return nil, err
+		}
+		paths = append(paths, subpaths...)
+	}
+	return paths, nil
 }
--- main_test.go
+++ main_test.go
@@ -1,11 +1,22 @@
-package fop_test
+package fop
 
 import (
+	"os"
 	"path/filepath"
+	"reflect"
 	"testing"
-
-	"github.com/tenkoh/fop"
 )
+
+func TestMain(m *testing.M) {
+	copyDst := "./testdata/CopyTree"
+	if err := os.RemoveAll(copyDst); err != nil {
+		return
+	}
+	if err := os.Mkdir(copyDst, 0777); err != nil {
+		return
+	}
+	m.Run()
+}
 
 func TestParentDir(t *testing.T) {
 	type args struct {
@@ -26,13 +37,100 @@ func TestParentDir(t *testing.T) {
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			got, err := fop.ParentDir(tt.args.path)
+			got, err := ParentDir(tt.args.path)
 			if (err != nil) != tt.wantErr {
 				t.Errorf("ParentDir() error = %v, wantErr %v", err, tt.wantErr)
 				return
 			}
 			if got != tt.want {
 				t.Errorf("ParentDir() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestWalkFiles(t *testing.T) {
+	type args struct {
+		dir string
+	}
+	tests := []struct {
+		name    string
+		args    args
+		want    []string
+		wantErr bool
+	}{
+		// TODO: Add test cases.
+		{
+			"without trailing slash",
+			args{"./testdata/WalkFiles"},
+			[]string{"testdata/WalkFiles/foo/bar/bar.txt", "testdata/WalkFiles/foo/foo.txt"},
+			false,
+		},
+		{
+			"with trailing slash",
+			args{"./testdata/WalkFiles/"},
+			[]string{"testdata/WalkFiles/foo/bar/bar.txt", "testdata/WalkFiles/foo/foo.txt"},
+			false,
+		},
+		{
+			"with file",
+			args{"./testdata/WalkFiles/foo/foo.txt"},
+			[]string{"testdata/WalkFiles/foo/foo.txt"},
+			false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := WalkFiles(tt.args.dir)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("WalkFiles() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("WalkFiles() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestCopyTree(t *testing.T) {
+	type args struct {
+		src string
+		dst string
+	}
+	tests := []struct {
+		name    string
+		args    args
+		wantErr bool
+	}{
+		// TODO: Add test cases.
+		{
+			"without trailing slash",
+			args{"./testdata/ParentDir", "./testdata/CopyTree"},
+			false,
+		},
+		{
+			"with trailing slash to dst",
+			args{"./testdata/ParentDir", "./testdata/CopyTree/"},
+			false,
+		},
+		{
+			"with trailing slash to src",
+			args{"./testdata/ParentDir/", "./testdata/CopyTree"},
+			false,
+		},
+		{
+			"with file",
+			args{"./testdata/ParentDir/hoge/piyo.txt", "./testdata/CopyTree"},
+			false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			_, err := CopyTree(tt.args.src, tt.args.dst)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("CopyTree() error = %v, wantErr %v", err, tt.wantErr)
+				return
 			}
 		})
 	}
